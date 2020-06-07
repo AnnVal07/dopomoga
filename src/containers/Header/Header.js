23db995c9e6a9737915c24f0019ec8b7a26cedb3
AnnVal07 <66559670+AnnVal07@users.noreply.github.com>
2020-06-07 18:34:31 +0300

    new
    ss

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,25 +1,31 @@
 import React from "react";
 import "./Header.sass";
+import { Menu } from 'antd';
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Row, Col } from 'antd';
 
 const Fragment = React.Fragment;
+const { SubMenu } = Menu;
 
 class Header extends React.Component{
 
     constructor(props) {
         super(props);
-        this.state = {
-
-        }
-
+        this.state = {}
     }
+
+    handleClick = e => {
+        console.log('click ', e);
+        this.setState({
+          current: e.key,
+        });
+    };
 
     render(){
         
         return(
             <Fragment>
-                <div className='header-content'>
-                    Header
-                </div>
+
             </Fragment>
         )
     }
